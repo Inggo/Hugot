54030d67577827c485e9dd218361da791c437054
Inggo Espinosa <inggo.espinosa@gmail.com>
2018-02-18 03:40:27 +0800

    Paginator, media styling

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,84 @@
+window.axios = require('axios');
+
 window.onload = function () {
-  var $siteHeaderBg = document.getElementsByClassName("site-header-bg")[0];
-  var $asideBg = document.getElementsByClassName("post-aside-bg").length > 0 ?
-  document.getElementsByClassName("post-aside-bg")[0] :
-  null;
+  var parallax = function () {
+    var $siteHeaderBgs = document.getElementsByClassName("site-header-bg");
+    var $asideBgs = document.getElementsByClassName("post-aside-bg");
 
-  var $siteImages = document.getElementsByTagName("img");
-  var $firstImg = $siteImages[0];
-  var $lastImg = $siteImages[$siteImages.length - 1];
+    for (var i = 0; i < $siteHeaderBgs.length; i++) {
+      $siteHeaderBgs[i].style.backgroundPosition = 'center calc(50% + ' + $siteHeaderBgs[i].getBoundingClientRect().top/4 + 'px)';
+    }
 
-  $siteHeaderBg.style.backgroundImage = 'url(' + $lastImg.src + ')';
-  $siteHeaderBg.style.opacity = '.25';
+    for (var j = 0; j < $asideBgs.length; j++) {
+      $asideBgs[j].style.backgroundPosition = 'center calc(50% - ' + $asideBgs[j].getBoundingClientRect().top/4 + 'px)';
+    }
+  };
 
-  if ($asideBg) {
-    $asideBg.style.backgroundImage = 'url(' + $firstImg.src + ')';
-    $asideBg.style.opacity = '.16';
+  function checkVisible (el) {
+    var rect = el.getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
   }
 
-  var parallax = function () {
-    $siteHeaderBg.style.backgroundPosition = 'center calc(50% + ' + window.scrollY/4 + 'px)';
-    if ($asideBg) {
-      $asideBg.style.backgroundPosition = 'center calc(50% + ' + $asideBg.getBoundingClientRect().top/4 + 'px)';
-    }
+  // var readMore = function () {
+  //   var $nextPage = document.getElementsByClassName("next-page");
+  //   if ($nextPage.length == 0) {
+  //     return;
+  //   }
+
+  //   for (var i = 0; i < $nextPage.length; i++) {
+  //     var el = $nextPage[i];
+
+  //     if (el.classList.contains('loading')) {
+  //       return;
+  //     }
+
+  //     if (!checkVisible(el)) {
+  //       return;
+  //     }
+
+  //     el.classList.add('loading');
+
+  //     var ld = document.createElement("i");
+  //     ld.classList.add("icon-spin");
+  //     ld.classList.add("animate-spin");
+  //     el.parentNode.insertBefore(ld, el.nextSibling);
+
+  //     console.log(el.href);
+  //     axios.get(el.href)
+  //       .then((response) => {
+  //         var nextPage = document.createElement("html");
+  //         nextPage.innerHTML = response.data;
+
+  //         if (document.getElementsByTagName("main").length == 0) {
+  //           nextPage.getElementsByTagName("h1")[0].remove();
+  //           var nextArticleHeader = nextPage.getElementsByTagName("header")[0];
+  //           var nextArticle = nextPage.getElementsByTagName("article")[0];
+  //           var currentArticle = el.parentNode.parentNode;
+  //           nextArticleHeader.classList.add("secondary-header");
+  //           nextArticleHeader.classList.remove("site-header");
+  //           currentArticle.parentNode.insertBefore(nextArticleHeader, currentArticle.nextSibling);
+  //           currentArticle.parentNode.insertBefore(nextArticle, nextArticleHeader.nextSibling);
+  //           el.parentNode.remove();
+  //         } else {
+  //           var nextArticles = nextPage.getElementsByTagName("article");
+  //           var pagination = nextPage.getElementsByClassName("pagination")[0];
+  //           nextPage.getElementsByClassName("prev-page")[0].remove();
+  //           var mainNode = el.parentNode.parentNode;
+  //           for (var i = 0; i < nextArticles.length; i++) {
+  //             mainNode.insertBefore(nextArticles[i], el.parentNode);
+  //           }
+  //           mainNode.insertBefore(pagination, el.parentNode);
+  //           el.parentNode.remove();
+  //         }
+  //       });
+  //   }
+  // };
+
+  window.onscroll = function () {
+    // readMore();
+    parallax();
   };
 
   parallax();
-
-  window.onscroll = function () {
-    if (window.scrollY > 70) {
-      
-    }
-    parallax();
-  };
 };
