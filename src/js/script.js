b951a2e8851cb78c08ae34cad2b1893a1a6e5d87
Inggo Espinosa <inggo.espinosa@gmail.com>
2018-02-23 00:38:56 +0800

    Add Google Site Search, Analytics, back to top

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+window.Velo = require('velocity-animate');
+
 window.axios = require('axios');
 
 window.onload = function () {
@@ -18,71 +20,27 @@ window.onload = function () {
     }
   };
 
-  function checkVisible (el) {
-    var rect = el.getBoundingClientRect();
-    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
-  }
+  var scrollClass = function () {
+    if (window.scrollY > window.innerHeight/2) {
+      document.getElementsByTagName('html')[0].classList.add("is-scrolled");
+    } else {
+      document.getElementsByTagName('html')[0].classList.remove("is-scrolled");
+    }
+  };
 
-  // var readMore = function () {
-  //   var $nextPage = document.getElementsByClassName("next-page");
-  //   if ($nextPage.length == 0) {
-  //     return;
-  //   }
-
-  //   for (var i = 0; i < $nextPage.length; i++) {
-  //     var el = $nextPage[i];
-
-  //     if (el.classList.contains('loading')) {
-  //       return;
-  //     }
-
-  //     if (!checkVisible(el)) {
-  //       return;
-  //     }
-
-  //     el.classList.add('loading');
-
-  //     var ld = document.createElement("i");
-  //     ld.classList.add("icon-spin");
-  //     ld.classList.add("animate-spin");
-  //     el.parentNode.insertBefore(ld, el.nextSibling);
-
-  //     console.log(el.href);
-  //     axios.get(el.href)
-  //       .then((response) => {
-  //         var nextPage = document.createElement("html");
-  //         nextPage.innerHTML = response.data;
-
-  //         if (document.getElementsByTagName("main").length == 0) {
-  //           nextPage.getElementsByTagName("h1")[0].remove();
-  //           var nextArticleHeader = nextPage.getElementsByTagName("header")[0];
-  //           var nextArticle = nextPage.getElementsByTagName("article")[0];
-  //           var currentArticle = el.parentNode.parentNode;
-  //           nextArticleHeader.classList.add("secondary-header");
-  //           nextArticleHeader.classList.remove("site-header");
-  //           currentArticle.parentNode.insertBefore(nextArticleHeader, currentArticle.nextSibling);
-  //           currentArticle.parentNode.insertBefore(nextArticle, nextArticleHeader.nextSibling);
-  //           el.parentNode.remove();
-  //         } else {
-  //           var nextArticles = nextPage.getElementsByTagName("article");
-  //           var pagination = nextPage.getElementsByClassName("pagination")[0];
-  //           nextPage.getElementsByClassName("prev-page")[0].remove();
-  //           var mainNode = el.parentNode.parentNode;
-  //           for (var i = 0; i < nextArticles.length; i++) {
-  //             mainNode.insertBefore(nextArticles[i], el.parentNode);
-  //           }
-  //           mainNode.insertBefore(pagination, el.parentNode);
-  //           el.parentNode.remove();
-  //         }
-  //       });
-  //   }
-  // };
+  document.getElementsByClassName('scroll-to-top')[0].onclick = function () {
+    Velo(
+      document.getElementsByTagName('html'),
+      "scroll",
+      { offset: 0, mobileHA: false }
+    );
+  };
 
   window.onscroll = function () {
-    // readMore();
+    scrollClass();
     parallax();
   };
 
+  scrollClass();
   parallax();
 };
